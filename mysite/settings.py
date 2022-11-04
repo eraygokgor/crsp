aaa8363b5d699afdc3170b578c09d14997fa371c
eraygokgor <eray.gokgor@apps.com.tr>
2022-11-04 16:40:07 +0300

    update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,13 +72,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'crsp_db',
+       'USER': 'postgres',
+       'PASSWORD': 'e_203G&57',
+       'HOST': 'database-2.cdylbm3uujb4.eu-central-1.rds.amazonaws.com',
+       'PORT': '5432',
+   }
+}
+
 
 
 # Password validation
